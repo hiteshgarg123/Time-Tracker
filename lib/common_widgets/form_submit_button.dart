71ee0b26748f29234f86caebf10c4f858a547089
Hitesh Garg <hiteshgargvk_cse18@its.edu.in>
2020-07-10 19:59:18 +0530

    Fixed typo

--- lib/common_widgets/form_submit_button.dart
+++ lib/common_widgets/form_submit_button.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:time_tracker/widgets/custom_raised_button.dart';
+import 'package:time_tracker/common_widgets/custom_raised_button.dart';
 
 class FormSubmitButton extends CustomRaisedButton {
   FormSubmitButton({
