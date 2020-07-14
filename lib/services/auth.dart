aef28005ea4d19fbc578b07d7d12f2ed157b4e52
Hitesh Garg <hiteshgargvk_cse18@its.edu.in>
2020-07-14 12:07:36 +0530

    Added photoUrl, display name, created User Avatar

--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -5,8 +5,14 @@ import 'package:flutter_facebook_login/flutter_facebook_login.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 
 class User {
-  User({@required this.uid});
+  User({
+    @required this.uid,
+    @required this.photoUrl,
+    @required this.displayName,
+  });
   final String uid;
+  final String photoUrl;
+  final String displayName;
 }
 
 abstract class AuthBase {
@@ -27,7 +33,11 @@ class Auth implements AuthBase {
     if (user == null) {
       return null;
     }
-    return User(uid: user.uid);
+    return User(
+      uid: user.uid,
+      displayName: user.displayName,
+      photoUrl: user.photoUrl,
+    );
   }
 
   @override
