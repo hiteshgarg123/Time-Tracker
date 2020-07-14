baecdf9312e1214402cffc5df4b94bfb69e986d1
Hitesh Garg <hiteshgargvk_cse18@its.edu.in>
2020-07-14 12:06:11 +0530

    Added and accessed current user

--- lib/app/home/account/account_page.dart
+++ lib/app/home/account/account_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:time_tracker/common_widgets/avatar.dart';
 import 'package:time_tracker/common_widgets/platform_alert_dialog.dart';
 import 'package:time_tracker/common_widgets/platform_exception_alert_dialog.dart';
 import 'package:time_tracker/services/auth.dart';
@@ -31,6 +32,7 @@ class AccountPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final user = Provider.of<User>(context, listen: false);
     return Scaffold(
       appBar: AppBar(
         title: Text('Account'),
@@ -46,7 +48,33 @@ class AccountPage extends StatelessWidget {
             onPressed: () => _confirmSignOut(context),
           ),
         ],
+        bottom: PreferredSize(
+          preferredSize: Size.fromHeight(130),
+          child: _buildUserInfo(user),
+        ),
       ),
+    );
+  }
+
+  Widget _buildUserInfo(User user) {
+    return Column(
+      children: <Widget>[
+        Avatar(
+          photoUrl: user.photoUrl,
+          radius: 50,
+        ),
+        SizedBox(
+          height: 10,
+        ),
+        if (user.displayName != null)
+          Text(
+            user.displayName,
+            style: TextStyle(color: Colors.white),
+          ),
+        SizedBox(
+          height: 10,
+        ),
+      ],
     );
   }
 }
