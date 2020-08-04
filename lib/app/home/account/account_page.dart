21aa030c6eeb06c6a3d83a7800be0bfc6ea3eb88
Hitesh Garg <hiteshgargvk_cse18@its.edu.in>
2020-08-04 21:03:15 +0530

    Methods to pick and upload avatar

--- lib/app/home/account/account_page.dart
+++ lib/app/home/account/account_page.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:provider/provider.dart';
+import 'package:time_tracker/app/home/models/avatar_reference.dart';
 import 'package:time_tracker/common_widgets/avatar.dart';
 import 'package:time_tracker/common_widgets/platform_alert_dialog.dart';
 import 'package:time_tracker/common_widgets/platform_exception_alert_dialog.dart';
 import 'package:time_tracker/services/auth.dart';
+import 'package:time_tracker/services/database.dart';
+import 'package:time_tracker/services/firebase_storage_service.dart';
+import 'package:time_tracker/services/image_picker_service.dart';
 
 class AccountPage extends StatelessWidget {
   Future<void> _signOut(BuildContext context) async {
@@ -30,6 +35,32 @@ class AccountPage extends StatelessWidget {
     }
   }
 
+  Future<void> _chooseAvatar(BuildContext context, User user) async {
+    try {
+      final imagePicker =
+          Provider.of<ImagePickerService>(context, listen: false);
+      final file = await imagePicker.pickImage(source: ImageSource.gallery);
+      if (file != null) {
+        final storage =
+            Provider.of<FirebaseStorageService>(context, listen: false);
+        final downloadUrl = await storage.uploadAvatar(file: file);
+        final database = Provider.of<Database>(context, listen: false);
+        await database.setAvatarReference(
+          AvatarReference(downloadUrl),
+          user.uid,
+        );
+        await file.delete();
+      }
+    } catch (e) {
+      print(e);
+      PlatformAlertDialog(
+        title: e.toString(),
+        content: 'Can\'t upload Image right now',
+        defaultActionText: 'Ok',
+      );
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     final user = Provider.of<User>(context, listen: false);
@@ -50,31 +81,44 @@ class AccountPage extends StatelessWidget {
         ],
         bottom: PreferredSize(
           preferredSize: Size.fromHeight(130),
-          child: _buildUserInfo(user),
+          child: _buildUserInfo(context, user),
         ),
       ),
     );
   }
 
-  Widget _buildUserInfo(User user) {
-    return Column(
-      children: <Widget>[
-        Avatar(
-          photoUrl: user.photoUrl,
-          radius: 50,
-        ),
-        SizedBox(
-          height: 10,
-        ),
-        if (user.displayName != null)
-          Text(
-            user.displayName,
-            style: TextStyle(color: Colors.white),
-          ),
-        SizedBox(
-          height: 10,
-        ),
-      ],
+  Widget _buildUserInfo(BuildContext context, User user) {
+    final database = Provider.of<Database>(context, listen: false);
+    return StreamBuilder<AvatarReference>(
+      stream: database.avatarReferenceStream(user.uid),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.active) {
+          final avatarReference = snapshot.data;
+          return Column(
+            children: <Widget>[
+              Avatar(
+                photoUrl: avatarReference?.downloadUrl,
+                radius: 50,
+                borderColor: Colors.black,
+                borderWidth: 2.0,
+                onPressed: () => _chooseAvatar(context, user),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              if (user.displayName != null)
+                Text(
+                  user.displayName,
+                  style: TextStyle(color: Colors.white),
+                ),
+              SizedBox(
+                height: 10,
+              ),
+            ],
+          );
+        }
+        return CircularProgressIndicator();
+      },
     );
   }
 }
